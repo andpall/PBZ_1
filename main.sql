4e5b14242d998694c543a0428416e76ae90a39a0
VD <Andpall5@gmail.com>
2020-09-28 13:51:16 +0300

    1

---
+++ main.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE lab1;
+USE lab1;
+CREATE TABLE teacher(
+id VARCHAR(5),
+lastname VARCHAR (20),
+position VARCHAR (20),
+department VARCHAR (50),
+specialty VARCHAR (50),
+homephone INT (5),
+PRIMARY KEY (id)
+);
---
+++ tmp.sql
@@ -0,0 +1,9 @@
+CREATE TABLE teacher(
+id VARCHAR(5),
+lastname VARCHAR (20),
+position VARCHAR (20),
+department VARCHAR (50),
+specialty VARCHAR (50),
+homephone INT (5),
+PRIMARY KEY (id)
+);
